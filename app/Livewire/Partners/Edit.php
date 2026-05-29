cf96dc7926a7ef883eeb9f70139b9c3cadd47c83
kelvinmurimi <murimikelvin229@gmail.com>
2026-05-29 19:18:44 +0300

    [Feature] Add Initial Partners Crud Files

---
+++ app/Livewire/Partners/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Partners;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.partners.edit');
+    }
+}
